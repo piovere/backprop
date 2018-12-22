fade1174be3372f113d5ad6c0b1783a989b5a4ea
J.R. Powers-Luhn <jpowersl@vols.utk.edu>
2018-12-21 20:50:25 -0500

    added bias value for each neuron

--- backprop/neuron.py
+++ backprop/neuron.py
@@ -4,9 +4,10 @@ import numpy as np
 class Neuron():
     def __init__(self, ninputs):
         self._weights = np.random.random(ninputs)
+        self._bias = 1.0
     
     def predict(self, x):
-        o = self._weights.dot(x)
+        o = self._weights.dot(x) + self._bias
         return self.transfer(o)
     
     def transfer(self, x):
