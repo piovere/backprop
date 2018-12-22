5e7b7bee0a1e397dc2a3acf596c6eba576068592
J.R. Powers-Luhn <jpowersl@vols.utk.edu>
2018-12-21 20:29:30 -0500

    layer consisting of many neurons

---
+++ backprop/layer.py
@@ -0,0 +1,10 @@
+import numpy as np
+from .neuron import Neuron
+
+
+class Layer():
+    def __init__(self, nneurons, ninputs):
+        self._neurons = [Neuron(ninputs) for i in range(nneurons)]
+    
+    def predict(self, x):
+        return np.array([n.predict(x) for n in self._neurons])
