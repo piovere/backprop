cbd4df787051bfab31ff356f01d3fe9dcd866ba7
J.R. Powers-Luhn <jpowersl@vols.utk.edu>
2019-01-01 14:47:22 -0500

    added output layer

--- backprop/layer.py
+++ backprop/layer.py
@@ -7,4 +7,14 @@ class Layer():
         self._neurons = [Neuron(ninputs) for i in range(nneurons)]
     
     def predict(self, x):
-        return np.array([n.predict(x) for n in self._neurons])
+        self._last_input = x
+        self._last_output = np.array([n.predict(x) for n in self._neurons])
+        return self._last_output
+
+
+class OutputLayer(Layer):
+    def __init__(self, ninputs):
+        super().__init__(1, ninputs)
+    
+    def error(self, y, yp):
+        return y - yp
