ddd4f64854fbe4d0fadc1986b52c73b3e28e33e2
J.R. Powers-Luhn <jpowersl@vols.utk.edu>
2021-02-27 21:42:33 -0800

    more coverage on a non-working library

--- tests/test_layer.py
+++ tests/test_layer.py
@@ -1,4 +1,4 @@
-from backprop.layer import Layer
+from backprop.layer import Layer, OutputLayer
 import numpy as np
 import pytest
 
@@ -24,3 +24,8 @@ def test_layer_predicts_correct_number_of_outputs(input):
     l = Layer(input, 5)
     r = l.predict(np.arange(5))
     assert r.shape[0] == input
+
+def test_output_layer_has_error():
+    ol = OutputLayer(5)
+    ol._neurons[0]._weights = np.zeros(5)
+    assert ol.error(1, 1) == 0
